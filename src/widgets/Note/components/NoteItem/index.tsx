b8950068b1264b9b2009d54022936a3fc3e149a0
Maksim Attsetski <maks2001maks2001@gmail.com>
2023-04-01 14:16:58 +0300

    add ability to edit and delete note

--- src/widgets/Note/components/NoteItem/index.tsx
+++ src/widgets/Note/components/NoteItem/index.tsx
@@ -1,17 +1,43 @@
-import React, { FC, memo } from 'react';
-import { INote } from 'widgets/Note';
+import React, { FC, memo, useState } from 'react';
+import { INote, useNote } from 'widgets/Note';
 
 import s from './NoteItem.module.scss';
 import { DateHelper } from 'shared';
+import { Button, Input } from 'UI';
 
 interface IProps {
   note: INote;
 }
 
 const NoteItem: FC<IProps> = ({ note }) => {
+  const { onEditNote, onDeleteNote } = useNote();
+
+  const [isEdit, setIsEdit] = useState<boolean>(false);
+  const [editText, setEditText] = useState<string>(note.text);
+
+  const onClickEdit = () => {
+    if (!isEdit) {
+      setIsEdit(true);
+    } else {
+      onEditNote({
+        ...note,
+        text: editText,
+        tags: editText.match(/#\w+/g) ?? [],
+      });
+      setIsEdit(false);
+    }
+  };
+
   return (
     <div className={s.note}>
-      <div>{note.text}</div>
+      {isEdit ? (
+        <Input text={editText} setText={setEditText} multiLines />
+      ) : (
+        <div>{note.text}</div>
+      )}
+      <br />
+      <Button onClick={onClickEdit}>{isEdit ? 'Save' : 'Edit'}</Button>
+      <Button onClick={() => onDeleteNote(note.id)}>Delete</Button>
       <br />
       <div>{DateHelper.getTimeString(note.createdAt)}</div>
     </div>
